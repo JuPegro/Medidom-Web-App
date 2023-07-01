03bdfa2247b723c9d45acfdc8fc169bed3e967f4
JuPegro <Majr1940@gmail.com>
2023-07-01 19:40:13 -0400

    Homapage | Home

--- components/Medidom.tsx
+++ components/Medidom.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCertificate, faHeartPulse, faHospital, faHospitalUser, faTooth, faUserDoctor } from '@fortawesome/free-solid-svg-icons'
+
 
 const Medidom = () => {
     return (
-        <div className="home">
+    <div className="home">
         <div className="home__content">
             <div className="info__text">
                 <h1>Cuidando tu salud, <span>en cada rincón.</span></h1>
@@ -20,38 +23,38 @@ const Medidom = () => {
             <Image src='/Home.png' alt='logo' width={700} height={700} className='doctor__img'/>
             <div className="circle__blur"></div>
             <div className="circle__tooth">
-                <i className='bx bx-injection'></i>
+                <FontAwesomeIcon icon={faTooth} beat className='tooth'/>
             </div>
             <div className="circle__heart">
-                <i className='bx bxs-heart'></i>
+                <FontAwesomeIcon icon={faHeartPulse} beatFade className='heart' />
             </div>
             <div className="info__medidom">
                 <div className="hospital__count">
-                    <i className='bx bx-plus-medical' ></i>
+                    <FontAwesomeIcon icon={faHospital} className='icon'/>
                     <h4>900</h4>
                     <p>Hospitales</p>
                 </div>
                 <div className="hospital__count">
-                    <i className='bx bx-plus-medical' ></i>
+                    <FontAwesomeIcon icon={faHospitalUser} className='icon'/>
                     <h4>20000+</h4>
                     <p>Pacientes</p>
                 </div>
                 <div className="hospital__count">
-                    <i className='bx bx-plus-medical' ></i>
+                    <FontAwesomeIcon icon={faUserDoctor} className='icon'/>
                     <h4>850</h4>
                     <p>Doctores</p>
                 </div>
                 <div className="hospital__count">
-                    <i className='bx bx-plus-medical' ></i>
+                    <FontAwesomeIcon icon={faCertificate} className='icon'/>
                     <h4>200</h4>
                     <p>Certificados</p>
                 </div>
             </div>
         </div>
         <div className="social__network">
-            <a href="#facebook"><i className='bx bxl-facebook-circle' ></i></a>
-            <a href="#instagram"><i className='bx bxl-instagram' ></i></a>
-            <a href="#twitter"><i className='bx bxl-twitter' ></i></a>
+            <a href="#facebook"><FontAwesomeIcon icon={faCertificate} className='icon'/></a>
+            <a href="#instagram"><FontAwesomeIcon icon={faCertificate} className='icon'/></a>
+            <a href="#twitter"><FontAwesomeIcon icon={faCertificate} className='icon'/></a>
         </div>
     </div>
     )
